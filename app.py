748f42feadcb0b09d1b6b3da79335982deaa02c4
djrayd3n <125121467+djrayd3n@users.noreply.github.com>
2023-08-19 11:00:38 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+# Prosty program do sumowania dwóch liczb
+
+# Prosimy użytkownika o wpisanie pierwszej liczby
+liczba1 = float(input("Wpisz pierwszą liczbę: "))
+
+# Prosimy użytkownika o wpisanie drugiej liczby
+liczba2 = float(input("Wpisz drugą liczbę: "))
+
+# Sumujemy obie liczby
+suma = liczba1 + liczba2
+
+# Wyświetlamy wynik
+print(f"Suma {liczba1} i {liczba2} wynosi: {suma}")
